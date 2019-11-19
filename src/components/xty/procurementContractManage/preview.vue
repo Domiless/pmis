56f69587732bc58a80c9e05db9fb60d9e9f3f50b
Domi Lee <251544193@qq.com>
2019-11-19 10:56:04 +0800

    修改打印预览

--- src/components/xty/procurementContractManage/preview.vue
+++ src/components/xty/procurementContractManage/preview.vue
@@ -7,19 +7,19 @@
         </a-row>
         <a-row>
             <a-col :span="12">
-                <span>供方：</span>
+                <span>供方：{{ contractMsg.supplierName }}</span>
             </a-col>
             <a-col :span="12">
-                <span>合同编号：</span>
+                <span>合同编号：{{ contractMsg.shopContractNo }}</span>
             </a-col>
             <a-col :span="12">
-                <span>需方：</span>
+                <span>需方：{{ contractMsg.demand }}</span>
             </a-col>
             <a-col :span="12">
-                <span>签订地点：</span>
+                <span>签订地点：{{ contractMsg.place }}</span>
             </a-col>
             <a-col :span="12">
-                <span>签订时间：</span>
+                <span>签订时间：{{ contractMsg.digndate }}</span>
             </a-col>
         </a-row>
         <a-row>
@@ -27,21 +27,21 @@
         </a-row>
         <a-row>
             <span class="msg_money">
-                <p>合计贷款：</p>
-                <p>合计税金：</p>
-                <p>合计人命币金额：</p>
+                <p>合计贷款：{{ contractMsg.summoney }}</p>
+                <p>合计税金：{{ contractMsg.taxMoney }}</p>
+                <p>合计人命币金额：{{ contractMsg.sumtaxMoney }}</p>
             </span>
             <span class="msg_chineseMoney">
-                <p>大写：</p>
-                <p>大写：</p>
-                <p>大写：</p>
+                <p>大写：{{ contractMsg.chineseMoney }}</p>
+                <p>大写：{{ contractMsg.chineseTaxMoney }}</p>
+                <p>大写：{{ contractMsg.chineseSumtaxMoney }}</p>
             </span>
             <p>（采购清单详见附件）</p>
         </a-row>
         <a-row>
             <a-col :span="24">
                 <p>二、质量要求技术标准、供方对质量负责的条件和期限：依照双方签订的《技术协议》、《质量赔偿协议》。</p> 
-                <p>三、交（提）货地点、方式：[--var--]。</p>
+                <p>三、交（提）货地点、方式：{{ contractMsg.place }}/{{ contractMsg.sendway }}。</p>
                 <p>四、运输方式及到达站港和费用负担：按经济快捷原则选定运输方式，由供负责运杂费及包装费。</p>
                 <p>五、合理损耗及计算方式：不得缺数短少，入库后原装短少由需方向供方索赔或在原货款中扣除。</p>
                 <p>六、包装标准，包装物的供应与回收：包装应能保护产品，经受运输、标志清楚、方便管理。包装物不回收。</p>
@@ -54,7 +54,7 @@
                 <p>十三、本合同由双方法定代表人或委托代理人签字或加盖双方合同专用章或公章生效。</p>
                 <p>十四、其它约定事项 ：如需方有合同变更或供方交货期发生变更，均应提前10-20天通知对方才有效;在合同有效期内如遇价格变动,按供需双方约定的时间，数量执行新价格。 </p>
                 <p>十五、供货方必须遵守国家环境保护的法律、法规，无重大环境事故发生，工业“四废”排放达到国家标准。</p>
-                <p>十六、合同有效期自： [--var--]  至 [--var--]  。</p>
+                <p>十六、合同有效期自： {{ contractMsg.validity.split(" ~ ")[0] }}  至 {{ contractMsg.validity.split(" ~ ")[1] }}  。</p>
             </a-col>
         </a-row>
         <a-row>
@@ -85,10 +85,55 @@
                 <a-col :span="8" class="msg_col msg_col3">
                     <p></p>
                     <p>鉴（公）证意见：</p>
+                    <p>经办人：</p>
+                    <p>鉴（公）证机关（章）：</p>
+                    <p>年 月 日</p>
                 </a-col>
             </a-col>
             <a-col :span="24">
                 <span>备注：本合同不得随意增减、删改，否则合同无效。本合同履行期间，双方以传真件、复印件、扫描件等方式签订的具体实施内容的合同与本合同具有同等法律效力。</span>
+            </a-col>
+        </a-row>
+        <div style="PAGE-BREAK-AFTER:always"></div>
+        <a-row>
+            <a-col :span="24">
+                <table class="table_contract">
+                    <tr>
+                        <td colspan="8" style="text-align: center">合同清单</td>
+                    </tr>
+                    <tr>
+                        <td colspan="8">
+                            <span>供方：SMC(中国)有限公司 10578</span>
+                            <span>合同编号：ZN-GG-1911 2830</span>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td colspan="8">
+                            <span>需方：四川长虹智能制造技术有限公司 10578</span>
+                            <span>签订时间：2019年11月5日</span>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>序号</td>
+                        <td>工作令</td>
+                        <td>名称型号</td>
+                        <td>单位</td>
+                        <td>数量</td>
+                        <td>不含税单价</td>
+                        <td>不含税总价</td>
+                        <td>交货时间</td>
+                    </tr>
+                    <tr></tr>
+                    <tr>
+                        <td colspan="6">合计：</td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td colspan="8">
+                            合计（不含13%增值税，含运费、贵司物流或快递至我司指定地点）：元
+                        </td>
+                    </tr>
+                </table>
             </a-col>
         </a-row>
     </div>
@@ -96,17 +141,27 @@
 <script>
 export default {
     data() {
-
+        return {
+            contractMsg: []
+        }
+    },
+    created() {
+        this.priviewType = sessionStorage.getItem("priviewType");
+        if (this.priviewType == 4) {
+			this.contractMsg = JSON.parse(sessionStorage.getItem("priview"));
+			console.log(this.contractMsg);
+		}
     }    
 }
 </script>
 <style lang="less">
 .preview {
     padding: 0px 5px;
-    background-color: white;
+    background-color: #ffffff;
     .title {
         font-size: 24px;
         text-align: center;
+        margin-bottom: 20px;
     }
     .msg_money {
         width: 40%;
@@ -117,7 +172,7 @@ export default {
         display: inline-block;
     }
     .msg_col {
-        border: 1px solid black;
+        border: 1px solid #000000;
     }
     .msg_col2 {
         border-left: 0px ;
@@ -129,6 +184,12 @@ export default {
      .msg_col3 {
         border-left: 0px ;
         height: 317px;
+    }
+    .table_contract {
+        width: 100%;
+        tr,td {
+            border: 1px solid #000000;
+        }
     }
 }
 </style>
